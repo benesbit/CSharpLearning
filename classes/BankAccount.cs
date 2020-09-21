219a0dbd03da91599ae180f5a3a129767f42da2a
Ben Nesbit <benesbit@yahoo.com>
2020-09-21 14:14:55 -0700

    Create logic for getting account balance.

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -8,7 +8,18 @@ namespace classes
         private static int accountNumberSeed = 1234567890;
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance { 
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            } 
+        }
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;

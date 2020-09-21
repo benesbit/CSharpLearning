b73c3972a4bd0c428432b1a742bf028cb1f5d1b7
Ben Nesbit <benesbit@yahoo.com>
2020-09-21 13:39:21 -0700

    Change initial deposit to a meme number. Hehehe.

--- classes/Program.cs
+++ classes/Program.cs
@@ -6,7 +6,7 @@ namespace classes
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Ben Nesbit", 10000);
+            var account = new BankAccount("Ben Nesbit", 42069);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
         }
     }
